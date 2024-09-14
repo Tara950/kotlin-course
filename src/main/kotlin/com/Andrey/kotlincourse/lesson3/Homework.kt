d065df93a550ed9ae02c6983f50e956804e2e7fe
Андрей <Djtitan95@gmail.com>
2024-09-14 22:36:05 +0700

    add file.txt

--- src/main/kotlin/com/Andrey/kotlincourse/lesson3/Homework.kt
+++ src/main/kotlin/com/Andrey/kotlincourse/lesson3/Homework.kt
@@ -1,4 +1,22 @@
 package com.Andrey.kotlincourse.lesson3
 
- class Homework {
- }
+val name: String = "Hackathon Survival"
+
+var date: String = "15.04.2025"
+
+val place: String = "St.Petersburg, Malaya Morskaya st., 10 "
+
+val budget: String = "250000"
+
+var participants: Int = 0
+    get() = field
+    private set(value) {field=value}
+
+var duration: Long = 12*60*60
+
+private lateinit var contactinformation: String
+
+var status: String = "not started"
+
+val notstarted: String = "list of sponsors"
+
