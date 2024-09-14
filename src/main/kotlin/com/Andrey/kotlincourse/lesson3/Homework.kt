6362e486183dc2c29137a81fed3c7d49600d1a69
Андрей <Djtitan95@gmail.com>
2024-09-15 04:02:52 +0700

    add file.txt

--- src/main/kotlin/com/Andrey/kotlincourse/lesson3/Homework.kt
+++ src/main/kotlin/com/Andrey/kotlincourse/lesson3/Homework.kt
@@ -14,8 +14,82 @@ var participants: Int = 0
 
 var duration: Long = 12*60*60
 
-private lateinit var contactinformation: String
+private var contactinformation: String = ""
 
 var status: String = "not started"
 
 val notstarted: String = "list of sponsors"
+
+var internetAccessLevel: Int = 4
+
+private var information: String = "Transportation information"
+
+var numberOfCommands: Int = 0
+
+val tasks: String = "Tasks"
+
+val evacuationPlan: String = "Evacuation plan"
+
+var listOfAvailableEquipment: String = "85"
+
+var listOfFreeEquipment: String = "85"
+
+val disasterPlan: String = "Disaster plan"
+
+lateinit var mealSchedule: String
+
+var listOfExpertsAndJury: String = ""
+
+val metod: String = ""
+
+var temperatureIndoor: Int = 26
+
+private val privacyPolicy: String = ""
+
+private val privateReviews: String by lazy { "" }
+
+val monitoring: String = ""
+
+var lightingLevel: Int = 1200
+
+private  val eventLog: String = ""
+
+val availabilityOfMedicalCare: Boolean = true
+
+val securityPlan: String = ""
+
+val eventRegistrationNumber: Int = 234367
+
+val maximumPermissibleNoiseLevel: Int = 75
+
+var noiseLevelIndicator: String = ""
+
+var eventFormat: String = ""
+
+var numberOfAvailableSeats: Int = 0
+    get() = field
+    set(value) {
+        if (value >= 0) field += value
+    }
+
+val pressRelationsPlan: String = ""
+
+private val projectInformation: String by lazy { "" }
+
+var permissionStatus: String = ""
+
+val exclusiveResources: Boolean = true or false
+
+val listPartners: String = ""
+
+lateinit var report: String
+
+val prizeDistributionPlan: String = ""
+
+private val contactInformation: String = "+123953848"
+
+val specialConditions: String = ""
+
+val maximumNumberOfPeople: Int = 150
+
+val  standardNumberOfHours: Int = 2*60*60
